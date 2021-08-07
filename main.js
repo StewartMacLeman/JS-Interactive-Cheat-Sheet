73f37e1164863c3912042c6785bdb68e5a45d2dc
Stewart MacLeman <stewart_macleman@hotmail.com>
2021-08-07 13:10:10 +0100

    Main styles and home page layout.

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 "use strict";
 
-console.log("hello there!");
+ // Global variables. -------------------------------------------
+ let navigationButton = document.querySelector(".navButton");
+
+ // Display / hide the navigation. ------------------------------
+ navigationButton.addEventListener("click", displayNavigation)
+
+ function displayNavigation(e){
+   let nextElement = e.target.nextElementSibling;
+   nextElement.classList.toggle("hide");
+
+   let button = e.target;
+   let buttonText = e.target.textContent;
+   let hide = "Hide Nav";
+   let show = "Show Nav";
+
+   if ( buttonText == "Show Nav" ){
+     button.textContent = hide;
+   } else if ( buttonText == "Hide Nav" ){
+     button.textContent = show;
+   }
+ }
